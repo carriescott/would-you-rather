55df1d7f689329217a3c00ce7cebbd1e5253ba8c
Carrie <carriescott0@gmail.com>
2020-04-06 12:39:43 +0100

    refactor Poll.js

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -3,9 +3,7 @@ import { connect } from 'react-redux';
 import QuestionForm from  './QuestionForm';
 import QuestionStats from './QuestionStats';
 import NoMatchFound from './NoMatchFound';
-import {Redirect} from "react-router-dom";
-
-//Import no match found component
+import { Redirect } from "react-router-dom";
 
 class Poll extends Component {
 
@@ -23,41 +21,39 @@ class Poll extends Component {
 
         const authedUser = this.props.authedUser;
         const questions = this.props.questions;
-
-        //set question from ID passed through from routing.
         const id = this.props.match.params.question_id;
-
-        //check if id exists within the question keys.
         const matchFound = Object.keys(questions).includes(id);
-        const users = this.props.users
-        const question = questions[id];
-        const author = question.author;
-        const authorObject = users[author];
-        const name = authorObject.name;
-        const pickeOptionOne = question.optionOne.votes.includes(this.props.authedUser);
-        const pickeOptionTwo = question.optionTwo.votes.includes(this.props.authedUser);
+        const users = this.props.users;
+        let isAnswered, questionObject;
 
-        console.log('authorObject', authorObject);
+        if (matchFound) {
+            const question = questions[id];
+            const author = question.author;
+            const authorObject = users[author];
+            const name = authorObject.name;
+            const pickeOptionOne = question.optionOne.votes.includes(this.props.authedUser);
+            const pickeOptionTwo = question.optionTwo.votes.includes(this.props.authedUser);
 
-        const questionObject = {
-            id: question.id,
-            author: name,
-            avatarURL: authorObject.avatarURL,
-            optionOneText: question.optionOne.text,
-            optionTwoText: question.optionTwo.text,
-            optionOneTotal: question.optionOne.votes.length,
-            optionTwoTotal: question.optionTwo.votes.length,
-            totalVotes: (question.optionOne.votes.length + question.optionTwo.votes.length),
-            optionOnePercent: ((question.optionOne.votes.length/(question.optionOne.votes.length + question.optionTwo.votes.length))*100),
-            optionTwoPercent: ((question.optionTwo.votes.length/(question.optionOne.votes.length + question.optionTwo.votes.length))*100),
-            pickedOptionOne: pickeOptionOne,
-            pickedOptionTwo: pickeOptionTwo
+            questionObject = {
+                id: question.id,
+                author: name,
+                avatarURL: authorObject.avatarURL,
+                optionOneText: question.optionOne.text,
+                optionTwoText: question.optionTwo.text,
+                optionOneTotal: question.optionOne.votes.length,
+                optionTwoTotal: question.optionTwo.votes.length,
+                totalVotes: (question.optionOne.votes.length + question.optionTwo.votes.length),
+                optionOnePercent: ((question.optionOne.votes.length/(question.optionOne.votes.length + question.optionTwo.votes.length))*100),
+                optionTwoPercent: ((question.optionTwo.votes.length/(question.optionOne.votes.length + question.optionTwo.votes.length))*100),
+                pickedOptionOne: pickeOptionOne,
+                pickedOptionTwo: pickeOptionTwo
+            }
+
+            isAnswered =
+                question.optionOne.votes.includes(authedUser) ||
+                question.optionTwo.votes.includes(authedUser);
         }
-
-        const isAnswered =
-            question.optionOne.votes.includes(authedUser) ||
-            question.optionTwo.votes.includes(authedUser);
-
+        
         let render = null;
 
         if (!matchFound) {
@@ -68,7 +64,6 @@ class Poll extends Component {
             render = <QuestionStats question={questionObject}/>
         }
 
-        //map over the users for each question in order to obtain name and avatar.
         return (
             <div>
                 {render}
@@ -76,7 +71,6 @@ class Poll extends Component {
         )
     }
 }
-
 
 function mapStateToProps ({authedUser, questions, users}) {
     return {
@@ -86,5 +80,4 @@ function mapStateToProps ({authedUser, questions, users}) {
     };
 }
 
-// invoke second function that is returned and passing it in Dashboard
 export default connect(mapStateToProps)(Poll)
