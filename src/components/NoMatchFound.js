a7a2f71da59893e3c99b02cf84e3227f845a0247
Carrie <carriescott0@gmail.com>
2020-04-06 12:31:19 +0100

    clean up NoMatchFound.js

--- src/components/NoMatchFound.js
+++ src/components/NoMatchFound.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
-//controlled component
 class NoMatchFound extends Component {
 
     render() {
         return (
-            <div className='center'>
-                <img src='https://gravatar.com/avatar/b9106a873e394fa182f827e720b43266?s=200&d=robohash&r=x'/>
+            <section className='center' id='no-match-found'>
+                <img src='https://gravatar.com/avatar/b9106a873e394fa182f827e720b43266?s=200&d=robohash&r=x'
+                    alt='No match found'
+                />
                 <p>Oops!</p>
                 <p>We can't find that one ... sorry, please try another one</p>
-            </div>
+            </section>
         )
     }
 }
 
-// invoke second function that is returned and passing it in Dashboard
 export default NoMatchFound
